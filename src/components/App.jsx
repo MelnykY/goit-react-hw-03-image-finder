c6e37d5076e12f6e5222545b2ef93268fb97be99
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-03-19 22:32:28 +0100

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,28 @@ import { GlobalStyles } from './GlobalStyles';
 import { StyledApp } from './App.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { fetchImages } from '../API/fetch';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Loader } from './Loader/Loader';
+import { Button } from './Button/Button';
 
+const Status = {
+      IDLE: 'idle',
+      SUCCESS: 'success',
+};
+    
 export class App extends Component {
   state = {
     page: 1,
     query: '',
+    status: Status.IDLE,
+    images: [],
+    isLoading: false,
+    isMore: false,
   };
 
-  handleSubmit = ({ query }, { resetForm }) => {
+handleSubmit = ({ query }, { resetForm }) => {
     this.setState({ query, page: 1 });
     resetForm();
   };
@@ -21,16 +35,73 @@ export class App extends Component {
     }));
   };
 
-  render() {
+  async componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
+
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({
+        isLoading: true,
+      });
+
+      try {
+        const data = await fetchImages(query, page);
+        if (data.hits.length === 0) {
+          this.setState({
+            status: Status.IDLE,
+          });
+          toast.error('No results were found for your request');
+          return;
+        }
+        this.setState({
+          isMore: data.hits.length === 12,
+        });
+        const images = data.hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => ({
+            id,
+            webformatURL,
+            largeImageURL,
+            tags,
+          })
+        );
+        if (prevState.query !== query) {
+          toast.success(`We found ${data.totalHits} images`);
+          this.setState({
+            status: Status.SUCCESS,
+            images: [...images],
+          });
+        } else {
+          this.setState({
+            images: [...prevState.images, ...images],
+          });
+        }
+        const totalPages = Math.ceil(data.totalHits / 12);
+        if (page === totalPages && page > 1) {
+          toast.info(`You reached end of results`);
+        }
+      } catch (error) {
+        toast.error('Sorry, something went wrong. Please, try again');
+        this.setState({
+          status: Status.IDLE,
+        });
+      } finally {
+        this.setState({
+          isLoading: false,
+        });
+      }
+    }
+  }
+
+  render() {
+    const { images, isLoading, isMore } = this.state;
     return (
       <StyledApp>
         <Searchbar onSubmit={this.handleSubmit} />
         <ImageGallery
-          query={query}
-          page={page}
+          images={images}
           handleCilck={this.handleCilck}
         />
+        {isLoading && <Loader visible={isLoading} />}
+        {isMore && <Button onClick={this.handleCilck} />}
         <GlobalStyles />
       </StyledApp>
     );
